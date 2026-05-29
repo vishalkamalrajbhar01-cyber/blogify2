8524039bd97656f77be7d93a1a9da6f6cfc8261a
vishalkamalrajbhar01-cyber <vishalkamalrajbhar01@gmail.com>
2026-05-29 12:29:31 +0530

    add

--- frontend/src/service/blog.js
+++ frontend/src/service/blog.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 
 
 export const addBlog = async (data) => {
-    let response = await axios.post('http://localhost:3000/addblog', data)
+    let response = await axios.post('https://blogify2-vbhx.onrender.com/addblog', data)
     return response;
 }
 
 
 export const getAllBlog = async () => {
-    let response = await axios.get('http://localhost:3000/allblog')
+    let response = await axios.get('https://blogify2-vbhx.onrender.com/allblog')
     return response;
 }
 
 export const getSingleBlogDetails = async (id) => {
-    let response = await axios.get(`http://localhost:3000/getblogbyid/${id}`)
+    let response = await axios.get(`https://blogify2-vbhx.onrender.com/getblogbyid/${id}`)
     return response;
 }
 
 export const editBlog = async (id, data) => {
-    let response = await axios.patch(`http://localhost:3000/editblog/${id}`, data)
+    let response = await axios.patch(`https://blogify2-vbhx.onrender.com/editblog/${id}`, data)
     return response;
 }
